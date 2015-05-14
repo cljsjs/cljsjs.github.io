87c7df928300722d4572cf149af3e89e1a9a716e
Juho Teperi <juho.teperi@iki.fi>
2015-05-14 18:56:31 +0300

    Added descriptions and homepage links

--- update.sh
+++ update.sh
@@ -12,12 +12,15 @@ for e in $(curl -s https://clojars.org/api/groups/cljsjs | jq -c ".[]"); do
     id="$group/$artifact"
     version=$(echo $e | jq ".latest_version")
     description=$(echo $e | jq -r ".description")
+    homepage=$(echo $e | jq -r ".homepage")
     echo "  <li>" >> $OUT
-    echo "    <a href=\"https://clojars.org/${id}\">${id}</a>" >> $OUT
+    echo "    <a href=\"https://clojars.org/${id}\">${artifact}</a>" >> $OUT
+    echo "    <a href=\"$homepage\" target=\"new\"><i class=\"fa fa-home\"></i></a>" >> $OUT
     echo "    <span class=\"clojars\">" >> $OUT
     echo "      <input type=\"text\" value='[${id} ${version}]'/>" >> $OUT
-    echo "      <button data-clipboard-text='[${id} ${version}]'>Copy</button>" >> $OUT
+    echo "      <button data-clipboard-text='[${id} ${version}]'><i class=\"fa fa-copy\"></i></button>" >> $OUT
     echo "    </span>" >> $OUT
+    echo "    <p class=\"description\">$description</p>" >> $OUT
     echo "  </li>" >> $OUT
 done
 
