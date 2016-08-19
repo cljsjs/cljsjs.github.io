6a106de522240579d2ec2c0eb49f1e41447aef07
Juho Teperi <juho.teperi@iki.fi>
2016-08-19 20:06:32 +0300

    Update gh-pages branch when updating data.json

--- deploy-update.sh
+++ deploy-update.sh
@@ -7,3 +7,24 @@ git reset --hard origin/real-code
 
 git commit -am "Automatic update"
 git push
+
+# like build.sh but copy only data.json
+
+rev=$(git rev-parse HEAD)
+remoteurl=$(git ls-remote --get-url origin)
+
+if [[ ! -d gh-pages ]]; then
+    git clone --branch master ${remoteurl} gh-pages
+fi
+(
+cd gh-pages
+git pull
+)
+
+cp -r resources/data.json gh-pages/data.json
+
+cd gh-pages
+
+git add --all
+git commit -m "Build from ${rev}."
+git push origin master
