8c083e41d0cdf0691c0091953944fc2c5968ad77
Martin Klepsch <martinklepsch@googlemail.com>
2016-09-14 15:30:36 +0200

    stash

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -7,6 +7,8 @@
 (defonce packages (r/atom nil))
 (defonce search (r/atom nil))
 
+(def cljsjs-group "cljsjs")
+
 (defn load-packages []
   (let [req  (js/XMLHttpRequest.)]
     (set! (.-responseType req) "json")
@@ -29,10 +31,9 @@
   (let [temp (r/atom @search)
         timeout (atom nil)]
     (fn []
-      [:input
-       {:class "twelve columns"
-        :type "text"
-        :placeholder "Search"
+      [:input.w-100.pa3
+       {:type "text"
+        :placeholder "Search ..."
         :value @temp
         :on-change (fn [e]
                      (let [v (.. e -target -value)]
@@ -44,14 +45,18 @@
                                                        300)))))}])))
 
 (defn select-on-click-input [text]
-  (let [el (atom nil)]
-    (fn [text]
-      [:input
-       {:type "text"
-        :default-value text
-        :ref #(reset! el %)
-        :on-click (fn [_]
-                    (if @el (.select @el)))}])))
+  (let [copied? (r/atom nil)]
+    (r/create-class
+     {:component-did-mount
+      (fn [this]
+        (js/Clipboard. (r/dom-node this)))
+      :reagent-render
+      (fn [text]
+        [:div.dim.pa3.bb.b--black-20
+         {:data-clipboard-text text
+          :on-click #(reset! copied? true)}
+         [:span.f4.code text]
+         [:span.ml1.f6.black-50 (if @copied? "(copied!)" "(click to copy)")]])})))
 
 (defn copy-button
   [_ & _]
@@ -67,16 +72,17 @@
            :data-clipboard-text text}]
          children))}))
 
+(defn dep-vec [artifact version]
+  (str "[" cljsjs-group "/" artifact " \"" version "\"]"))
+
 (defn package-list []
   (let [query @search]
     [:ul
      (for [{:keys [artifact description homepage version deps]} @(r/track filtered-packages)]
-       (let [group_name "cljsjs"
-             id (str group_name "/" artifact)
-             dependency-vector (str "[" id " \"" version "\"]")]
+       (let [dependency-vector (dep-vec artifact version)]
          [:li
           {:key artifact}
-          [:a {:href (str "https://clojars.org/" id)}
+          [:a {:href (str "https://clojars.org/" cljsjs-group "/" artifact)}
            [hi/highlight-string artifact query]]
           " "
           [:a {:href (str "https://github.com/cljsjs/packages/tree/master/" artifact) :target "new"} [:i.fa.fa-book]]
@@ -90,11 +96,46 @@
           [:p.description [hi/highlight-string description query]]
           [:pre.deps deps]]))]))
 
+(defn package-list' []
+  (let [query @search]
+    [:ul.list.pl0
+     (for [{:keys [artifact description homepage version deps]} @(r/track filtered-packages)]
+       (let [dependency-vector (dep-vec artifact version)]
+         [:li.ba.mb3.b--black-20
+          {:key artifact}
+          ;; [:a {:href (str "https://clojars.org/" cljsjs-group "/" artifact)}
+          ;;  [hi/highlight-string artifact query]]
+          ;; " "
+          ;; [:a {:href (str "https://github.com/cljsjs/packages/tree/master/" artifact) :target "new"} [:i.fa.fa-book]]
+          ;; " "
+          ;; [:a {:href homepage :target "new"} [:i.fa.fa-home]]
+
+          [:div
+           [select-on-click-input dependency-vector]
+           #_[copy-button
+            {:text dependency-vector}
+            [:i.fa.fa-copy]]]
+
+          [:p.pa3.ma0 [hi/highlight-string description query]]
+
+          [:div.cf.bt.b--black-20
+           [:a {:href homepage :target "new"}
+            [:div.fl-ns.w-33-ns.w-100.pv2.ph3.br-ns.dim "Project Site"]]
+           [:a {:href (str "https://github.com/cljsjs/packages/tree/master/" artifact) :target "new"}
+            [:div.fl-ns.w-33-ns.w-100.pv2.ph3.br-ns.dim "Package Readme"]]
+           [:a {:href (str "https://clojars.org/" cljsjs-group "/" artifact)}
+            [:div.fl-ns.w-33-ns.w-100.pv2.ph3.dim "Clojars"]]]
+          ;; project site
+          ;; package readme
+          ;; clojars
+
+          #_[:pre.deps deps]]))]))
+
 (defn main []
   [:div
    [:h3 "Packages"]
    [search-input]
-   [package-list]])
+   [package-list']])
 
 (defn init! []
   (load-packages)
